5342d6bbcc3b0e7733790b1c00c81130543ec9fd
Daniel Gonzalez Lareo <danigonza86@gmail.com>
2013-02-27 01:09:47 +0100

    Initial commit of an example of heroku app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuApp::Application.config.secret_token = '6336b676a98148dab76a25e1de67672899832bec7b3db27885fc6137614d719e80c8dd47458f48e711cde762a72722e95f4ab4706ea7cf320ee78ef8b15193e8'
